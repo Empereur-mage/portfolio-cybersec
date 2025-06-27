b65610ca6842501b2456b1101811fbe0a712a6f3
Empereur-mage <medstart4@gmail.com>
2025-06-27 17:07:47 +0100

    Premier commit portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+// Script JS pour futures interactions (animations, formulaire, etc.)
+
+//  scroll doux vers les sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
